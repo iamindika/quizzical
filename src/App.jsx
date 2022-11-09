31f7b001848879a3fa5ac70e0727fe5e5917be44
Indika Tantrege <indika.kolamba@ca3cmam0f177wmd.ht.home>
2022-11-09 13:46:56 -0500

    add body styles
    utilize blob assets in background

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import blueBlob from "./assets/blue-blob.png";
+import yellowBlob from "./assets/yellow-blob.png";
+
 function App() {
   return (
-    <>
+    <main className="main">
+      <img 
+        className="blob--left"
+        src={blueBlob}
+        alt=""
+      />
+      <img 
+        className="blob--right"
+        src={yellowBlob}
+        alt=""
+      />
       Quizzical
-    </>
+    </main>
   )
 }
 
