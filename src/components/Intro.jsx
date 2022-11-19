f3783fd65b10c74770efd483ec5c8176c53277a8
Indika Tantrege <indika.kolamba@ca3cmam0f177wmd.ht.home>
2022-11-18 20:13:17 -0500

    add wrapper class, remove container class and add image assets

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,12 +1,27 @@
+import blueBlob from "../assets/blue-blob.png";
+import yellowBlob from "../assets/yellow-blob.png";
+
 const Intro = ({ onStart }) => (
-  <main className="intro container">
-    <h1 className="intro-title">Quizzical</h1>
-    <p className="intro-desc">Test your knowledge in various categories, with varying degrees of difficulty!</p>
-    <button 
-      className="intro-btn"
-      onClick={onStart}
-    >Start Quiz</button>
-  </main>
+  <div className="wrapper">
+    <main className="intro container">
+      <h1 className="intro-title">Quizzical</h1>
+      <p className="intro-desc">Test your knowledge in various categories, with varying degrees of difficulty!</p>
+      <button 
+        className="intro-btn"
+        onClick={onStart}
+      >Start Quiz</button>
+    </main>
+    <img 
+      className="blob--left"
+      src={blueBlob}
+      alt=""
+      />
+    <img 
+      className="blob--right"
+      src={yellowBlob}
+      alt=""
+      />
+  </div>
 );
 
 export default Intro;
