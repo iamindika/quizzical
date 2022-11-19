2b492b397b0ebdd0c449e2b2a85afef4cead11d6
Indika Tantrege <indika.kolamba@ca3cmam0f177wmd.ht.home>
2022-11-18 20:12:21 -0500

    define handle start and remove image assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,17 @@
 import { useState } from "react";
 import Intro from "./components/Intro";
 import Quiz from "./components/Quiz";
-import blueBlob from "./assets/blue-blob.png";
-import yellowBlob from "./assets/yellow-blob.png";
 
 function App() {
   const [ startQuiz, setStartQuiz ] = useState(false);
 
-  return (
-    <>
-      <img 
-        className="blob--left"
-        src={blueBlob}
-        alt=""
-        />
-      <img 
-        className="blob--right"
-        src={yellowBlob}
-        alt=""
-        />
-        { startQuiz 
-          ? <Quiz />
-          : (<Intro 
-              onStart={() => setStartQuiz(true)} 
-            />)}
-    </>
-  )
+  const handleStart = () => setStartQuiz(true)
+
+  return startQuiz 
+    ? <Quiz />
+    : <Intro 
+        onStart={handleStart} 
+      />
 }
 
 export default App
